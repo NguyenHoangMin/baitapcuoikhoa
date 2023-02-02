6d6376095a56464ed8ab7bd8278b136f7b58913b
NguyenHoangMin <hoangminh1234b@gmail.com>
2023-02-02 16:58:16 +0700

    first commit

---
+++ c.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  // 👇️ hide button
+  btn.style.display = 'none';
+
+  // 👇️ show div
+  const box  = document.getElementById('box');
+  box.style.display = 'block';
+  const box2 = document.getElementById('box2');
+  box2.style.display = 'block';
+  const box3 = document.getElementById('box3');
+  box3.style.display = 'block';
+  const box4 = document.getElementById('box4');
+  box4.style.display = 'block';
+  const box5 = document.getElementById('box5');
+  box5.style.display = 'block';
+  const box6 = document.getElementById('box6');
+  box6.style.display = 'block';
+  const box7 = document.getElementById('box7');
+  box7.style.display = 'block';
+  const box8 = document.getElementById('box8');
+  box8.style.display = 'block';
+});
